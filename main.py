406470c61a9561832e7c0049c6fedc8ae0f9d0a5
Katyanna Moura <amelie.kn@gmail.com>
2019-10-27 18:07:27 +0100

    Basic structure for the website

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "PyLadies Berlin (:"
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
