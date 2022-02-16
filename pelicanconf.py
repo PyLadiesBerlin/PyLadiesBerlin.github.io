da40e04d5e24c755b56ed96e7c7860c5fbb236c8
Meili Triantafyllidi <meili.tr@gmail.com>
2022-02-16 21:41:35 +0100

    add images path in static
    it seems that after adding a STATIC_PATH variable, then all the paths
    to static have to be added when RELATIVE_URLS = False at least

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ PAGE_PATHS = ['pages/en']
 ARTICLE_PATHS = ['blog/en']
 
 # extra static
-STATIC_PATHS = ['static']
+STATIC_PATHS = ['static', 'images']
 EXTRA_PATH_METADATA = {
     'static/favicon.ico': {'path': 'favicon.ico'},
     }
