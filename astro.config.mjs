2cdfc7f52f6a6f5c00a8ec4f5a239779f691724b
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2025-02-09 01:50:44 +0100

    Update vercel/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from '@playform/compress';
 
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
+  // Must be 'static' or 'hybrid'
+  output: 'static',
+  adapter: vercelStatic(),
 });
